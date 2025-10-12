cb7a9d742a77d29726b4dbf8ba64752251315816
Anvitha H M <54534166+anvithahm@users.noreply.github.com>
2025-10-12 16:59:10 +0530

    Create WORLD-11.sql

---
+++ WORLD-11.sql
@@ -0,0 +1,12 @@
+/*Greece has capital Athens.
+
+Each of the strings 'Greece', and 'Athens' has 6 characters.
+
+Show the name and capital where the name and the capital have the same number of characters.
+
+You can use the LENGTH function to find the number of characters in a string
+For Microsoft SQL Server the function LENGTH is LEN*/
+
+SELECT name, capital
+  FROM world
+ WHERE length(name)=length(capital);
