988ca4e6f12e72bfd0534e6df56982762521e50f
Anvitha H M <54534166+anvithahm@users.noreply.github.com>
2025-10-15 18:55:54 +0530

    Create LeetC7.sql

---
+++ LeetC7.sql
@@ -0,0 +1,3 @@
+--Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+
+select P.product_name, S.year, S.price from Sales S left join Product P on S.product_id=P.product_id;
